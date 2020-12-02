92e80aa4eaa64803d309a39e9ce9785f35b6420a
Sam9613 <72975708+Sam9613@users.noreply.github.com>
2020-12-02 20:45:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,10 @@ box1=new Box(400,100,50,50);
 
 box2=new Box(410,120,60,20);
 box3=new Box(420,140,40,40);
-
+box4=new Box(450,100,50,50);
+box5=new Box(460,120,60,20);
+box6=new Box(470,140,40,40);
+ball2=new Ball(450,10,40);
 
 Engine.run(myengine)
 }
@@ -27,7 +30,10 @@ function draw() {
  box2.show();
  box3.show();
  ball.show();
-
+ box4.show();
+ box5.show();
+ box6.show();
+ball2.show();
 
 
   drawSprites();
